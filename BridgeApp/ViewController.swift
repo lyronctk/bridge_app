138ab75b15162a78fe39e2d689c498ebdfc90518
Lyron Co Ting Keh <lyronctk@gmail.com>
2019-11-22 19:10:56 -0800

    initial commit

---
+++ BridgeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BridgeApp
+//
+//  Created by Lyron Co Ting Keh on 11/22/19.
+//  Copyright © 2019 Lyron Co Ting Keh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
